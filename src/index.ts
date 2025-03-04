ad035bffcabe22e085aa8d97c090f187756a9580
Alexander Valencia <alex16jpv@gmail.com>
2025-03-03 22:22:31 -0500

    update wait utility function to set a default delay of 1000ms

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,23 @@
 import { ApiError, CustomError } from "./shared/errors";
+import { wait } from "./shared/utils";
 
-try {
-  throw new ApiError("NotFound");
-} catch (error: unknown) {
-  if (error instanceof ApiError) {
-    console.error(error.name);
-    console.error(error.message);
-    console.error(error.statusCode);
-    console.log(error.details);
-  } else {
-    console.error(error);
+const start = async () => {
+  console.time("WAIT");
+  await wait(500);
+  console.timeEnd("WAIT");
+
+  try {
+    throw new ApiError("NotFound");
+  } catch (error: unknown) {
+    if (error instanceof ApiError) {
+      console.error(error.name);
+      console.error(error.message);
+      console.error(error.statusCode);
+      console.log(error.details);
+    } else {
+      console.error(error);
+    }
   }
-}
+};
+
+start();
