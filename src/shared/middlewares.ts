2161608f030f2eb72f7fe2ffd27c1b7ccf690593
Alexander Valencia <alex16jpv@gmail.com>
2025-03-04 23:53:51 -0500

    feat: enhance error middleware to handle Sequelize unique constraint validation errors

--- src/shared/middlewares.ts
+++ src/shared/middlewares.ts
@@ -1,6 +1,10 @@
 import { NextFunction, Request, Response } from "express";
 import { ApiError } from "./errors";
 
+interface ValidationError extends Error {
+  errors: { message: string }[];
+}
+
 export const errorMiddleware = (
   error: Error,
   _: Request,
@@ -15,6 +19,19 @@ export const errorMiddleware = (
     });
     return;
   }
+
+  if (
+    error?.name === "SequelizeUniqueConstraintError" &&
+    (error as ValidationError)?.errors?.length > 0
+  ) {
+    res.status(400).json({
+      error: "ValidationError",
+      message: (error as ValidationError).errors
+        ?.map((err: any) => err.message)
+        ?.join(", "),
+    });
+  }
+
   res.status(500).json({
     error: "InternalServerError",
     message: error.message,
